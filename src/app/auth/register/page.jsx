df27b0b1519e466d1179e8823685d5e4e4686183
sanjana-moon <sanjanamoonliya@gmail.com>
2026-05-01 21:28:40 +0600

    better auth implemented

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,8 +1,7 @@
 'use client'
-import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 
-const LoginPage = () => {
+const RegisterPage = () => {
 
     const {
         register,
@@ -19,10 +18,31 @@ const LoginPage = () => {
         <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100 p-10">
             <div className="p-20 rounded-xl bg-white">
                 <h2 className="font-bold text-3xl text-center mb-6">
-                    Login to your account
+                    Register your account
                 </h2>
 
                 <form className="space-y-4" onSubmit={handleSubmit(handleLogicFunc)}>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Name</legend>
+                        <input
+                            type="text"
+                            className="input w-full"
+                            placeholder="Enter your name"
+                            {...register("name", { required: "Name is required" })}
+                        />
+                        {errors.name && (<p className='text-red-500'>{errors.name.message}</p>)}
+                    </fieldset>
+
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Photo URL</legend>
+                        <input
+                            type="text"
+                            className="input w-full"
+                            placeholder="Enter your photo url"
+                            {...register("name", { required: "Name is required" })}
+                        />
+                        {errors.name && (<p className='text-red-500'>{errors.name.message}</p>)}
+                    </fieldset>
 
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Email</legend>
@@ -41,19 +61,18 @@ const LoginPage = () => {
                             type="password"
                             className="input w-full"
                             placeholder="Enter your password"
-                            {...register("password", { required: "Password is required" })}
+                            {...register("email", { required: "Email is required" })}
                         />
-                        {errors.password && (<p className='text-red-500'>{errors.password.message}</p>)}
+                        {errors.email && (<p className='text-red-500'>{errors.email.message}</p>)}
                     </fieldset>
 
                     <button className="btn w-full bg-slate-800 text-white">
-                        Login
+                        Register
                     </button>
-                    <p className='mt-3 text-center'>Dont’t Have An Account ? <Link href={'/auth/register'} className='text-blue-500'>Register</Link></p>
                 </form>
             </div>
         </div>
     );
 };
 
-export default LoginPage;
+export default RegisterPage;
--- src/component/homePage/TopTilesSection.jsx
+++ src/component/homePage/TopTilesSection.jsx
@@ -6,9 +6,9 @@ const TopTilesSection = async () => {
     const topCards = await cards.slice(0, 4);
 
     return (
-        <div className="container mx-auto my-20">
+        <div className="container mx-auto mb-20 p-6 border-b pb-20 border-gray-300">
             <h3 className="text-3xl mb-6 font-bold text-[#384959]">Our Top Collection:</h3>
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
                 {
                     topCards.map(card => <Cards key={card.id} card={card} />)
                 }
